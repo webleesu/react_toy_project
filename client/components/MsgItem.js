009fa3c104809314be1ec717fc4f70711d533d28
[webleesu] <[websumin@gmail.com]>
2023-04-01 17:00:19 +0900

    새 글 쓰기, 메시지 수정, 삭제

--- client/components/MsgItem.js
+++ client/components/MsgItem.js
@@ -1,5 +1,16 @@
-const MsgItem = ({ userId, timestamp, text }) => (
-  <li className="messages__itmes">
+import MsgInput from "./MsgInput";
+
+const MsgItem = ({
+  id,
+  userId,
+  timestamp,
+  text,
+  onUpdate,
+  onDelete,
+  isEditing,
+  startEdit,
+}) => (
+  <li className="messages__item">
     <h3>
       {userId}{" "}
       <sub>
@@ -14,7 +25,18 @@ const MsgItem = ({ userId, timestamp, text }) => (
       </sub>
     </h3>
 
-    {text}
+    {isEditing ? (
+      <>
+        <MsgInput mutate={onUpdate} text={text} id={id} />
+      </>
+    ) : (
+      text
+    )}
+
+    <div className="messages__buttons">
+      <button onClick={startEdit}>수정</button>
+      <button onClick={onDelete}>삭제</button>
+    </div>
   </li>
 );
 
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,10 @@
-import MsgList from "../components/MsgList"
+import MsgList from "../components/MsgList";
 
 const Home = () => (
   <>
     <h1>SIMPLE SNS</h1>
     <MsgList />
   </>
-)
+);
 
-export default Home
+export default Home;
