96a2d9d59f6ca8279a41244d99de9df4c017a1ce
[webleesu] <[websumin@gmail.com]>
2023-04-03 20:32:40 +0900

    server rest api
    data id관련 404 에러

---
+++ server/src/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import messagesRoute from "./routes/messages.js";
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+messagesRoute.forEach(({ method, route, handler }) => {
+  app[method](route, handler);
+});
+
+app.listen(8000, () => {
+  console.log("server listening on 8000 ...");
+});
